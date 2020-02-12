a34763a5f3b080e579f7c81e3db6de482b0754a4
Ole Sivert Aarhaug <ole.sivert@aarhaug.com>
2020-02-12 20:41:00 +0100

    Network driver is ish working

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
+use std::sync::mpsc::channel;
+use std::string;
+use std::thread;
+
 mod net_io;
 pub mod elev_io;
 
 use net_io::*;
 
 fn main() {
-    println!("Hello, world!");
+    let (sending_tx, sending_rx) = channel::<std::vec::Vec<u8>>();
+    let (reciving_tx, reciving_rx) = channel::<std::vec::Vec<u8>>();
+    let net = thread::spawn(move || {
+        let mut network = Communication::new("localhost".to_string(), 12345, sending_rx, reciving_tx).unwrap();
+        network.start();
+    });
+    sending_tx.send(vec![0,0,0,1]).unwrap();
+    loop {
+        println!("Got something! {:?}", reciving_rx.recv().unwrap())
+    }
+    net.join();
 }
--- src/net_io.rs
+++ src/net_io.rs
@@ -1,37 +1,37 @@
 use std::io;
 use std::net::TcpStream;
 use std::io::{Write, Read};
-use std::thread;
-use std::sync::mpsc::channel;
 use std::sync::mpsc::{Receiver, Sender};
 
 pub struct Communication {
     stream: TcpStream,
-    pub tx: Sender<u8>,
+    data_to_send: Receiver<std::vec::Vec<u8>>,
+    data_to_recive: Sender<std::vec::Vec<u8>>
 }
 
 impl Communication {
-    pub fn new(ip: String, port: u8, tx: Sender<u8>) -> io::Result<Self> {
+    pub fn new(ip: String, port: u16, send_message: Receiver<std::vec::Vec<u8>>, receive_message: Sender<std::vec::Vec<u8>>) -> io::Result<Self> {
         let connect_ip: String = ip + ":" + &port.to_string();
-        let mut tcp_stream = TcpStream::connect(connect_ip)?;
+        let tcp_stream = TcpStream::connect(connect_ip)?;
 
-        Ok(Communication{ stream: tcp_stream, tx: tx})
-    }
-    pub fn send(&mut self, buffer: &[u8]) -> io::Result<()> {
-        self.stream.write(buffer)?;
-        Ok(())
+        Ok(Communication{ stream: tcp_stream, data_to_send: send_message, data_to_recive: receive_message})
     }
 
-    pub fn listen(&mut self) {
-        let mut readBuffer = [0 ; 9];
+    pub fn start(&mut self){
         loop {
-            match self.stream.read(&mut readBuffer) {
-                Ok(size) => {
-                    
+            match self.data_to_send.recv() {
+                Ok(data) => {
+                    let mut buffer = [0; 1024];
+                    let _ = self.stream.write(&data.into_boxed_slice());
+                    let _ = self.stream.read(&mut buffer);
+                    let mut t = Vec::new();
+                    for i in buffer.iter() { t.push(*i); }
+                    let _ = self.data_to_recive.send(t);
                 }
-                Err(_) => return,
+    
+                Err(_) => return, // This means, that the sender has disconnected
+                                  // and no further messages can ever be received
             }
-            
         }
     }
 }
